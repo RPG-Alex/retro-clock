509763d4a219ba05f091daf43aacdc570ac371d2
Alex Blood <mralexblood@outlook.com>
2024-01-16 15:56:12 +0800

    Updated Readme and cleaned up main

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# retro-clock
-A simple graphical clock based on Tim McNara's tutorial. 
+<a name="readme-top"></a>
+
+# Bevy Clock Gizmo
+
+<div align="center"> 
+    <img src="images/logo.png" alt="Logo"> 
+</div>
+
+<details> 
+    <summary>Contents</summary>
+    <ol> 
+        <li> 
+            <a href="#introduction">Introduction</a> 
+        </li>
+        </li> 
+        <li> 
+            <a href="#installation-and-setup">Installation and Setup</a> 
+        </li> 
+        <li> 
+            <a href="#usage">Usage</a> 
+        </li> 
+        <li> 
+            <a href="#contributing">Contributing</a> 
+        </li> 
+        <li> 
+            <a href="#license">License</a> 
+        </li> 
+        <li> 
+            <a href="#acknowledgments">Acknowledgments</a> 
+        </li> 
+    </ol> 
+</details>
+
+## Introduction
+
+Welcome to Bevy Clock Gizmo! This project is a testament to the power of the Bevy engine, a modern, efficient Rust framework for building graphical tools. Inspired by Tim McNamara's insightful YouTube video on Bevy's capabilities, this project aims to demonstrate the versatility of Rust in creating visually appealing applications.
+
+[Tim McNamara's video](https://www.youtube.com/watch?v=jhI69fodWUY&ab_channel=timClicks) showcases the potential of Bevy in a way that's both educational and inspiring. For more on Tim's work, check out his [LinkedIn profile](https://www.linkedin.com/in/timmcnamaranz/). 
+
+In this project, we've created a clock gizmo using Bevy's powerful tools. The clock face is dynamically rendered with customizable colors and responsive design, offering a visually appealing experience. This project is ideal for those interested in exploring the capabilities of Rust and Bevy in graphical programming, and aren't sure where to start.
+
+## Installation and Setup
+
+Make sure you have rust installed. Beyond that dynamic linking is enabled in the `Cargo.toml` file by default, so disable if you do not want to use that feature (bevy recommended)
+
+## Usage
+
+```bash
+cargo run
+```
+
+## Contributing
+
+Any and all contributions are welcome. This was a simple project. I may try to grow it I may not, will welcome all PR's and issues. 
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Acknowledgments
+
+A special thanks to Tim McNamara for his invaluable insights into Rust and Bevy. His YouTube video was a key inspiration for this project. The Bevy community is also greatly appreciated for their support and contributions to the Rust ecosystem.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // sauce: https://www.youtube.com/watch?v=jhI69fodWUY&ab_channel=timClicks
 
 use bevy::prelude::*;
-use chrono::{NaiveDateTime,Timelike};
+use chrono::Timelike;
 
 fn main() {
     App::new()
@@ -19,13 +19,12 @@ fn setup(
 
     // set the line thickness
     gizmo_conf.line_width = 10.0;
+    // Generate Text field
     commands.spawn(TextBundle::from_section("PLACEHOLDER", TextStyle { 
         font: default() , font_size: 14.0, color: Color::WHITE })
             .with_style(Style {
                 position_type: PositionType::Absolute,
-                //bottom: Val::Percent(50.0),
                 right: Val::Percent(47.5),
-                //left: Val::Percent(50.0),
                 top: Val::Percent(50.0),
                 ..default()
             }))
@@ -33,25 +32,30 @@ fn setup(
 
 }
 
+// Passes in the gizmos, queries text, and queries window size
 fn clock_face(
     mut gizmos: Gizmos,
     mut query: Query<&mut Text>,
     mut window: Query<&mut Window>,
 ) {
+    // Get time and setup time
     let now = chrono::Local::now();
     let hour =  now.hour() as f32;
     let minute = now.minute() as f32;
     let second = now.second() as f32;
 
+    // Create our circle arc
     let hour_angle = ((360.0/ 24.0) * hour).to_radians();
     let minute_angle = ((360.0/60.0) * minute).to_radians();
     let second_angle = ((360.0/60.0) * second).to_radians();
 
+    // Share time in readable manner
     let disp_time = now.time().format("%H:%M:%S");
-    let disp_date = now.date_naive().format("%Y-%B-%d");
+    //let disp_date = now.date_naive().format("%Y-%B-%d");
 
-    //set Seconds location
+    // Get the display info for relative locations
     let sec_loc = window.single_mut();
+    //set Seconds location for 4 corners
     let (top_left_x, top_left_y) = (sec_loc.width()/-3.5, sec_loc.height()/3.5);
     let (top_right_x, top_right_y) = (sec_loc.width()/3.5, sec_loc.height()/3.5);
     let (bottom_left_x, bottom_left_y) = (sec_loc.width()/-3.5, sec_loc.height()/-3.5);
@@ -61,24 +65,17 @@ fn clock_face(
     gizmos.arc_2d(Vec2::ZERO, hour_angle / 2.0, hour_angle, 100., Color::BISQUE).segments(360*3);
     // minutes
     gizmos.arc_2d(Vec2::ZERO, minute_angle / 2.0, minute_angle, 120., Color::TEAL).segments(360*3);
-    // seconds
+    // seconds (main output)
     gizmos.arc_2d(Vec2::ZERO, second_angle / 2.0, second_angle, 140., Color::ORANGE).segments(360*3);
     
-    
-	// gizmos.arc_2d(Vec2::ZERO, second_angle / 2.0, second_angle, 200., Color::SILVER).segments(360*3);
-    
-    // gizmos.arc_2d(Vec2::ZERO, second_angle / 2.0, second_angle, 240., Color::YELLOW).segments(360*3);
-	// gizmos.arc_2d(Vec2::ZERO, second_angle / 2.0, second_angle, 260., Color::CRIMSON).segments(360*3);
-    // gizmos.arc_2d(Vec2::ZERO, second_angle / 2.0, second_angle, 280., Color::AQUAMARINE).segments(360*3);
-
+    // Instantiate the gizmos for the 4 corners second counters
     gizmos.arc_2d(Vec2::new(top_left_x, top_left_y), second_angle / 2.0, second_angle, 60., Color::LIME_GREEN).segments(360*3);
     gizmos.arc_2d(Vec2::new(top_right_x, top_right_y), second_angle / 2.0, second_angle, 60., Color::BLUE).segments(360*3);
     gizmos.arc_2d(Vec2::new(bottom_left_x,bottom_left_y), second_angle / 2.0, second_angle, 60., Color::VIOLET).segments(360*3);
     gizmos.arc_2d(Vec2::new(bottom_right_x,bottom_right_y), second_angle / 2.0, second_angle, 60., Color::CYAN).segments(360*3);
+
+    //output time to text field
     for mut text in &mut query {
         text.sections[0].value = format!("{disp_time}");
-        //text.sections[0].value = format!("{disp_date}");
-        
     }
-
 }
